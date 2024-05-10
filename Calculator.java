e7b795e7cbd28778a1900250d76a89fc97aad4e0
Evinjohn11 <evinjohnignatious@gmail.com>
2024-05-10 22:19:42 +0530

    sdasd

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Enter the operation (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Error: Invalid operation. Please enter +, -, *, or /.");
+                return;
+        }
+
+        System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
+
+        scanner.close();
+    }
+}
